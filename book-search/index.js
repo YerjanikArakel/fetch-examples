3a67efac02f9e1b1f1372a27eacd3d47adbc71ee
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-08-10 23:04:41 +0400

    pushing with each folders

---
+++ book-search/index.js
@@ -0,0 +1,6 @@
+const input = document.querySelector(".searchInput");
+const btn = document.querySelector("#searchBtn");
+
+btn.addEventListener("click", function () {
+  console.log(input.value);
+});
